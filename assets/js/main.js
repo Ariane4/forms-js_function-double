b3ebe0ec63858179c7ba289eecd8ffa61f08d6a3
Ariane4 <abischoff48@gmailcom>
2020-02-25 10:42:48 +0100

    Start

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+// function mulitply (Nummer){
+//     return Nummer * 2
+// }
+// console.log(Nummer[2])
+
+function multi(){
+    let ergebnis = document.getElementById("Nummer").value * 2;
+    document.getElementById("Ergebnis").innerHTML = ergebnis;
+}
